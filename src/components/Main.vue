4ff716bce73bd02afccf68d8f47437ec61071edb
Ottavio42 <ottaviog88@gmail.com>
2021-10-14 11:50:50 +0200

    main

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <Hero />
+  </main>
+</template>
+
+<script>
+import Hero from "./Hero.vue";
+export default {
+  name: "Main",
+  components: {
+    Hero,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+</style>
