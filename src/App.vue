857ccfdca0ee213beb70460cbcfaaeeeac635411
Ottavio42 <ottaviog88@gmail.com>
2021-10-14 11:51:36 +0200

    Hero-appVue-commonScss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :headerNav="headerNav" />
+    <Main />
+    <Footer :footerNav="footerNav" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "@fortawesome/fontawesome-free/js/all.js";
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer,
+    Main,
+  },
+  data() {
+    return {
+      headerNav: ["HOME", "ABOUT", "PRICES", "COURSES", "BLOG", "BOOK NOW"],
+      footerNav: {
+        information: [
+          {
+            word: "123456 North Main Street, New York, NY 555555",
+            fawesome: "fas fa-home",
+          },
+          {
+            word: "0.003.965.98",
+            fawesome: "fas fa-phone",
+          },
+          {
+            word: "info-yourdomain.com",
+            fawesome: "fas fa-envelope",
+          },
+          {
+            word: "theme-fusion.com",
+            fawesome: "fas fa-link",
+          }
+        ],
+        courses: ["Pass Plus", "Intensive Course", "Automatic", "Istructor Training"]
+
+      }
+    }
   }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// Common
+@import "./assets/style/common.scss";
+// Variables
+@import "./assets/style/variables.scss";
+// Font
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;700;800&display=swap');
+
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="hero">
+    <div class="hero-cont">
+      <div class="hero-text">
+        <h1>Drive with Avada</h1>
+        <p>We offer the finest driving tutorial money can buy</p>
+      </div>
+      <div class="hero-button">
+        <a href="#">
+          <span><i class="fas fa-id-card-alt"></i></span>
+          <div>Demos</div>
+        </a>
+        <a href="#">
+          <span id="btn-verde">$39</span>
+          <div>On sale</div>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+.hero{
+  height: 500px;
+  background-image: url("../assets/img/homepage-hero-background.jpg");
+  background-size: cover;
+  position: relative;
+  z-index: 0;
+  .hero-cont{
+    width: $sizeCont;
+    margin: auto;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .hero-text{
+      color: #fff;
+      h1{
+        font-size: 1.875rem;
+      }
+      p{
+        font-size: 1.25rem;
+      }
+    }
+    .hero-button{
+      position: absolute;
+      right: 1rem;
+      top: 10rem;
+      a{
+        display: block;
+        text-decoration: none;
+        color: #33475b;
+        background-color: #fff;
+        border-radius: .625rem;
+        text-align: center;
+        margin-bottom: .625rem;
+        font-size: .75rem;
+        padding: .3125rem;
+        span{
+          font-size: 1.25rem;
+        }
+      }
+      #btn-verde{
+            color: #7abc64;
+          }
+    }
+  }
+
+}
+
+</style>
